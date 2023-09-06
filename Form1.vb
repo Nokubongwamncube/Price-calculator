bf71bf982e8f943c5fd8f044e1861a429986c551
Dell-User <Dell-User@DESKTOP-4LJVQAT>
2023-09-07 01:06:17 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles Txtquantity.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'declaring necessray varaibles
+        Dim price As Decimal
+        Dim quantity As Decimal
+        Dim total As Decimal
+
+        'getting the amount from the textboxes
+
+        price = Txtprice.Text
+        quantity = Txtquantity.Text
+
+        'calculating the total
+
+        total = price * quantity
+
+        'Dsiplaying message on the screen
+
+        MsgBox("total for customers is R" & total)
+
+    End Sub
+End Class
